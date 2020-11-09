2eed966cc8f9b0a5a5efd89d588690b387003483
a381953656 <381953656@qq.com>
2020-11-09 17:21:29 +0800

    备注信息

---
+++ miniprogram/pages/zeng/zeng.js
@@ -0,0 +1,77 @@
+// pages/zeng/zeng.js
+const db = wx.cloud.database();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    db.collection('product').get({
+      success(res){
+        //添加数据成功的回调函数
+        console.log('查询数据成功',res)
+      },
+      fail(res){
+        //添加函数失败的回调函数
+        console.log('查询数据失败',res)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
